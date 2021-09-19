82718aca288300e9a512fba998abf95ef504a807
beomki69 <beomki69@gmail.com>
2021-09-19 17:28:17 +0900

    refactor: Change reducer using createReducer - redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,22 +1,18 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const TODO_LIST = "TODO_LIST";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  console.log(state, action);
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const initState = JSON.parse(localStorage.getItem(TODO_LIST)) || [];
 
